dea0db80953ef6cec630f1227f0b3f8ac8431526
Саша Волков <49091865+Lexv0lk@users.noreply.github.com>
2025-04-06 18:45:51 +0300

    добавил тутор

---
+++ Assets/Game/Scripts/Utilities/SceneChangeButton.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Game.Scripts.Utilities
+{
+    [RequireComponent(typeof(Button))]
+    public class SceneChangeButton : MonoBehaviour
+    {
+        [SerializeField] private string _sceneName;
+        
+        private Button _button;
+
+        private void Awake()
+        {
+            _button = GetComponent<Button>();
+        }
+
+        private void OnEnable()
+        {
+            _button.onClick.AddListener(OnButtonClicked);
+        }
+        
+        private void OnDisable()
+        {
+            _button.onClick.RemoveListener(OnButtonClicked);
+        }
+
+        private void OnButtonClicked()
+        {
+            SceneManager.LoadScene(_sceneName);
+        }
+    }
+}
